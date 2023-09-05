c8fae28735a242ab5aa1bd6c65e7d74e93e6cb2a
Esteban <cepalacios@outlook.com>
2023-09-05 17:43:47 -0500

    Add basic element layout and distribution of elements

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+import showPoster from '../assets/poster-placeholder.png';
+import closeBtn from '../assets/close-btn.png';
+
+const showPosterImg = document.getElementById('show-poster');
+const PopupCloseBtn = document.getElementById('close-btn');
+showPosterImg.src = showPoster;
+PopupCloseBtn.src = closeBtn;
