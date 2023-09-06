c28d407f29f45b28b419d98b651f53fbdf1ad50f
alex1779 <alex1779@gmail.com>
2023-09-06 15:16:06 -0300

    Adding modification according to the reviewers.

--- src/index.js
+++ src/index.js
@@ -1,46 +1,14 @@
 import './style.css';
 import heart from '../assets/heart.png';
+import createCards from './cards.js';
+import populateCards from './utils.js';
 
-const fullPath = 'https://api.tvmaze.com/shows';
+createCards();
 
 // Loading images heart like
 const imgHearts = document.getElementsByClassName('cardboard-heart-image');
+Array.from(imgHearts).forEach((item) => {
+  item.src = heart;
+});
 
-for (let i = 0; i < imgHearts.length; i += 1) {
-  imgHearts[i].src = heart;
-}
-
-const getAllShows = async () => {
-  try {
-    const response = await fetch(fullPath);
-    if (!response.status) {
-      throw new Error('Failed to create a post.');
-    }
-    const leaderboardResponse = await response.json();
-    return leaderboardResponse;
-  } catch (error) {
-    return error.message;
-  }
-};
-
-const result = await getAllShows();
-const qtyShows = result.length;
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
-const selectedIndex = [];
-for (let i = 0; i < 9; i += 1) {
-  selectedIndex.push(getRandomInt(qtyShows));
-}
-
-const selectedShows = [];
-selectedIndex.forEach((element) => selectedShows.push(result[element]));
-
-const imgCards = document.getElementsByClassName('cardboard-image');
-const titleCards = document.getElementsByClassName('cardboard-title');
-for (let i = 0; i < titleCards.length; i += 1) {
-  titleCards[i].innerHTML = selectedShows[i].name;
-  imgCards[i].src = selectedShows[i].image.medium;
-}
+populateCards();
