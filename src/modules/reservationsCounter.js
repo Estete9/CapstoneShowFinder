4337f078a6faa50cee5d4428151a0ac070249e99
Alvee <M.Alvee8141@gmail.com>
2023-09-08 23:13:20 +0600

    small fixes

---
+++ src/modules/reservationsCounter.js
@@ -0,0 +1,1 @@
+export const reservationsCounter = (reservations) => reservations.length;
--- src/tests/reservations.test.js
+++ src/tests/reservations.test.js
@@ -1,11 +1,14 @@
-jest.mock('../modules/reservations');
-
-const { reservationsCounter } = require('../../__mocks__/reservations');
-
-const dummyReservations = [{ end_date: '3/2/2023', start_date: '34/4/2003', item_id: 3 }, { end_date: '3/2/2023', start_date: '34/4/2003', item_id: 2 }, { end_date: '3/2/2023', start_date: '34/4/2003', item_id: 1 }];
+import {reservationsCounter} from "../modules/reservationsCounter";
 
 describe('Reservations Test', () => {
   test('Reservations counter should give total count', () => {
+
+    const dummyReservations = [
+      { end_date: '3/2/2023', start_date: '34/4/2003', item_id: 3 }, 
+      { end_date: '3/2/2023', start_date: '34/4/2003', item_id: 2 }, 
+      { end_date: '3/2/2023', start_date: '34/4/2003', item_id: 1 }
+    ];
+    
     expect(reservationsCounter(dummyReservations)).toStrictEqual(3);
   });
 });
