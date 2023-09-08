64a9edfc8d3f475ace302e2499006e68681950f3
Alvee <M.Alvee8141@gmail.com>
2023-09-08 19:23:52 +0600

    Create reservations.test.js

---
+++ src/tests/reservations.test.js
@@ -0,0 +1,12 @@
+jest.mock('./index');
+
+const { reservationsCounter } = require('../modules/reservations');
+
+const dummyReservations = [ {end_date: '3/2/2023', start_date: "34/4/2003", item_id: 3},{end_date: '3/2/2023', start_date: "34/4/2003", item_id: 2},{end_date: '3/2/2023', start_date: "34/4/2003", item_id: 1}]
+
+describe('Reservations Test', () => {
+  test('Reservations counter should give total count', () => {
+    expect(reservationsCounter(dummyReservations)).toStrictEqual(3);
+  });
+
+});
